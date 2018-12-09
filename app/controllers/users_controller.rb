d6d6c6369ddc307e814572736012edc57d6548a0
marina8915 <nonalatus@ukr.net>
2018-12-09 22:39:22 +0200

    fixed user role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :access, :role)
+    params.require(:user).permit(:name, :email, :password, :access)
   end
 end
